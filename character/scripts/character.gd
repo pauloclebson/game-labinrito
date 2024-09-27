b2f92c63aada429f1d3c3a9855b17dc48e4760de
pauloclebson <pauloclebson270993@gmail.com>
2024-09-27 10:20:02 -0300

    ajustes no script

--- character/scripts/character.gd
+++ character/scripts/character.gd
@@ -1,45 +1,16 @@
 extends CharacterBody2D
 
-var _state_machine
+@export var speed := 35
+@export var target : Area2D
+@onready var navigation: NavigationAgent2D = $NavigationAgent2D
 
-@export_category("Variables")
-@export var _move_speed: float = 64.0
-
-@export var _friction: float = 0.2
-@export var _acceleration: float = 0.2
-
-@export_category("Objects")
-@export var _animation_tree: AnimationTree = null
-
-func _ready() -> void:
-	_state_machine = _animation_tree["parameters/playback"]
-	
-func _physics_process(_delta: float) -> void:
-	_move()
-	_animate()
+func _physics_process(delta: float) -> void:
+	var dir = to_local(navigation.get_next_path_position()).normalized()
+	velocity = dir * speed
 	move_and_slide()
+	
+func go_to_target():
+	navigation.target_position = target.global_position
 
-func _move() -> void:
-	var _direction: Vector2 = Vector2(
-		Input.get_axis("move_left", "move_right"),
-		Input.get_axis("move_up", "move_down")
-	)
-	
-	if _direction != Vector2.ZERO:
-		_animation_tree["parameters/idle/blend_position"] = _direction
-		_animation_tree["parameters/walk/blend_position"] = _direction
-		velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _acceleration)
-		velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _acceleration)
-		return
-	
-	velocity.x = lerp(velocity.x, _direction.normalized().x * _move_speed, _friction)
-	velocity.y = lerp(velocity.y, _direction.normalized().y * _move_speed, _friction)
-	velocity = _direction.normalized() * _move_speed
-	
-func _animate() -> void:
-	if velocity.length() > 1:
-		_state_machine.travel("walk")
-		return
-	
-	_state_machine.travel("idle")
-		
+func _on_timer_timeout() -> void:
+	go_to_target()
